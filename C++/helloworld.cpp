fefe88543ce6d625758d887b2de408d0214dc5da
HannahBagtasos <hannahvistabagtasos@gmail.com>
2024-03-17 22:15:42 +0000

    Adding Neetcode
    Adding Neetcode

---
+++ C++/helloworld.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(){
+    std::cout << "I like sushi" << std::endl; //endline, flush the endpoint buffer
+    std::cout << "Testing 123" << '\n';
+
+    int x;//declaration
+    x = 5;
+
+    std :: cout << x << std::endl;
+    return 0; //means no problem
+}
